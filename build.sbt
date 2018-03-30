14cfe1d3ba649322fcb880d3b396d10fd6e9f3d9
vladkanash <vlad.kanash@expert-soft.by>
2018-03-30 14:55:35 +0300

    Added sample main class.

--- build.sbt
+++ build.sbt
@@ -8,3 +8,5 @@ libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % Test
 libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.13.4" % Test
+
+mainClass := Some("Main")
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,17 @@
+import entity.{Chunk, Grid}
+
+object Main extends App {
+  val grid = new Grid(5,4)
+
+  val chunks = List(
+    Chunk(List((0,0), (0,1), (1,0))),
+    Chunk(List((0,0), (0,1), (1,0))),
+    Chunk(List((0,0), (0,1), (1,0))),
+    Chunk(List((0,0), (0,1), (1,0))),
+    Chunk(List((0,0), (0,1), (1,0), (1,1))),
+    Chunk(List((0,0), (1,0), (2,0), (3,0)))
+//    Chunk(List((0,0), (1,0), (2,0), (3,0), (4,0)))
+  )
+
+  println(grid.placeChunks(chunks))
+}
